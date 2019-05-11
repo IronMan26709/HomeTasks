c919c5934b1091c7629706b37717ff79d8673d1c
IronMan <ironmanandrej@gmail.com>
2019-05-11 14:39:54 +0300

    Убрал цикл forEach на простой цикл и методды строк

--- Lesson-02/hw 02/script.js
+++ Lesson-02/hw 02/script.js
@@ -1,17 +1,26 @@
 ////////////////////////////////////////////   First task    //////////////////////////////// 
+// function dropString (string){	
+// 	var latters = [ ]; 
+// 	var stringSplit = string.split ( ' ' ) ;
+// 	stringSplit.forEach( function(i){
+// 		var a = i[0];
+// 		latters.push (a);
+// 		}	
+// 	)
+// 	console.log(latters);
+// 	console.log(latters.join(' '))
+// }
+// dropString("Backend As A Service")
+
+
 function dropString (string){	
 	var latters = [ ]; 
-	var stringSplit = string.split ( ' ' ) ;
-	stringSplit.forEach( function(i){
-		var a = i[0];
-		latters.push (a);
-		}	
-	)
+	var stringSplit = string.split ( ' ' ) ;		
+	for( var i = 0; i < stringSplit.length; i++) latters.push (stringSplit[i].charAt(0));
 	console.log(latters);
 	console.log(latters.join(' '))
 }
 dropString("Backend As A Service")
-
 ///////////////////////////////////////  Second task     ///////////////////////////////////////////
 function check (x){
 	var newData = new Date()
