0c36521f7ab622131ecb23f41280a3d843c0327c
IronMan <ironmanandrej@gmail.com>
2019-05-12 22:53:22 +0300

    сделал 5пункт самостоятельной

--- Lesson-02/Самостоятельная/script.js
+++ Lesson-02/Самостоятельная/script.js
@@ -40,9 +40,22 @@ for( var i = 0; i < group.length  ; i++ ){
 		 var stud =`${group[i].name} ${group[i].lastName} ${group[i].age} ${group[i].notebook}`;
 		 console.log(stud)
 	}	
-  function addNewStud(name, lastName, age, notebook) {
-  	var group = group.push ( );
-  	console.log(group)
-  }
+  function addNewStud( name, lastName, age, notebook) {
+     group.push({
+        name: name,
+        lastName: lastName, 
+        age: age,
+        notebook: notebook
+    })
+}
   
-addNewStud("вася", "чебурашкин",651 , false )
+addNewStud("вася", "чебурашкин",651 , false )	
+
+//////////////////////////////////////////////////////  Задание 5   ///////////////////////////////////////////////////////////////////////
+var names = ["Khadija","Lee","Alicia","Lachlan","Margie","Isabelle","Ellie-May"];
+var surname = ["Holmes","Reed","Hunt","Duncan","Singh","Gardiner","Nelson"];
+
+for(var i = 0; i < names.length; i++){
+        var concat = `${names[i]} ${surname[i]}`
+        console.log(concat);
+   }
