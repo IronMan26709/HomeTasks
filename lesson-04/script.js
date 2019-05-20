7e0308ca20663041beaa07740f359dad34f0f3f0
IronMan <ironmanandrej@gmail.com>
2019-05-20 14:04:33 +0300

    Сделал 4,3

--- lesson-04/script.js
+++ lesson-04/script.js
@@ -1,14 +1,37 @@
 /////////////////////////////////////////////// FirstPart  //////////////////////////////////////////////////////////////////////
 var ladiesHandBar = {
-    Косметичка:"Sea To Summit",
-    Кошелек:"ST Leather",
-    Ключи:"От квартиры",
-    Добавить: function addNewItem (newKey, newValue){
+    cosmeticBag:"Sea To Summit",
+    purse:"ST Leather",
+    keys:"От квартиры",
+    addProp: function addNewItem (newKey, newValue){
        ladiesHandBar[newKey] = newValue 
     },
-    Удалить: function (item){
+    delProp: function (item){
        delete ladiesHandBar[item] 
     }
 }
-
-////////////////////////////////////////////////
+console.log(ladiesHandBar)
+////////////////////////////////////////////////   ThirdPart    //////////////////////////////////////////////////////////////////
+function Const (name, age, hobby) {
+    this. name = name;
+    this.age = age;
+    this.hobby = hobby
+}
+Const. prototype. addProperty = function (newProp, newValue ){
+    this[newProp] = newValue
+}
+Const.prototype.delProperty = function(prop){
+    delete this[ prop ]
+}
+var person1 = new Const("Vasya", 55, "striptiz")
+console.log(person1)
+var person2 = new Const("Valya", 16, "Chess")
+console.log(person2)
+///////////////////////////
+console.log("       Добавляем свойство объекту")
+person2.addProperty("salary", 12558)
+console.log(person2)
+///////////////////////////
+console.log("       Удаляем свойство у объкта")
+person2.delProperty("salary")
+console.log(person2)
